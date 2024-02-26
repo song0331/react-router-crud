4d8c4cdc951a5a4507938e9cc0f3bcec0d749b34
song0331 <thdgusrb@gmail.com>
2024-02-26 23:59:09 +0900

    [add] NavLink로 현재 페이지 표시

--- src/Layout/NavBar/NavBar.jsx
+++ src/Layout/NavBar/NavBar.jsx
@@ -1,34 +1,41 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 function NavBar() {
   return (
-    <ul>
-      <li>
-        <Link to='/' aria-label='홈으로 이동'>
-          홈
-        </Link>
-      </li>
-      <li>
-        <Link to='/change' aria-label='교환 페이지로 이동'>
-          교환
-        </Link>
-      </li>
-      <li>
-        <Link to='/meetup' aria-label='밋업 페이지로 이동'>
-          밋업
-        </Link>
-      </li>
-      <li>
-        <Link to='/mypage' aria-label='마이페이지로 이동'>
-          마이페이지
-        </Link>
-      </li>
-      <li>
-        <Link to='/error' aria-label='에러페이지로 이동'>
-          Error 페이지
-        </Link>
-      </li>
-    </ul>
+    <nav>
+      <ul>
+        <li>
+          <NavLink to='/' className={({ isActive }) =>
+            isActive ? "active" : ""} aria-label='홈으로 이동'>
+            홈
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/change' className={({ isActive }) =>
+            isActive ? "active" : ""} aria-label='교환 페이지로 이동'>
+            교환
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/meetup' className={({ isActive }) =>
+            isActive ? "active" : ""} aria-label='밋업 페이지로 이동'>
+            밋업
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/mypage' className={({ isActive }) =>
+            isActive ? "active" : ""} aria-label='마이페이지로 이동'>
+            마이페이지
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/error' className={({ isActive }) =>
+            isActive ? "active" : ""} aria-label='에러페이지로 이동'>
+            Error 페이지
+          </NavLink>
+        </li>
+      </ul>
+    </nav >
   )
 }
 
